da83588fe729e164ac496ad11dbc85376e557137
Blake Rose <bdecktor@gmail.com>
2019-04-22 01:17:00 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif puts "Hi Nana".upcase
+  elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
   else "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
