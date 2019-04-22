3e96a2523470e879085cc9470d630bf0ea846057
Blake Rose <bdecktor@gmail.com>
2019-04-22 01:06:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(x)
   if x.downcase
     then return "HUH?! SPEAK UP, SONNY!"
-  elsif argument.upcase
+  elsif x.upcase
     then return "NO, NOT SINCE 1938!"
   else "I LOVE YOU GRANDMA!"
     then return "I LOVE YOU TOO PUMPKIN!"
